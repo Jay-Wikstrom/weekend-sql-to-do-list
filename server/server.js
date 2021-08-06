a550d7a7520a2f8eb2ffc28da7c9cfac7374f9e1
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-08-05 20:28:03 -0500

    Setup server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const router = require('./routes/router');
+const PORT = 5000;
+
+//Setup body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('server/public'));
+
+// Serve back static files by default
+app.use(express.static('server/public'))
+
+//Connect Routes after router setup
+
+
+//Start listening for requests on port 5000
+app.listen(PORT, () => {
+    console.log('listening on port', PORT)
+})
