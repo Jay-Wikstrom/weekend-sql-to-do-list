9e44b848c3556deffacd986bb4afad66486e75cd
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-08-05 21:47:42 -0500

    Setup AJAX on client side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,9 +4,38 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('On ready test');
-    setupClickListeners();
+    $(document).on('click', '.submitBtn', postTaskData);
 }
 
-function setupClickListeners(){
-    console.log('Click listeners');
+function getTaskData(){
+    $.ajax({
+        type: 'GET',
+        url: '/todo'
+    }).then(function(response){
+        console.log('The resonse is', response);
+
+        for (let i = 0; i < response.length; i++){
+            $('#toDoList').append(`
+                <tr>
+                    <td>${response[i].task}</td>
+                </tr>
+            `);
+        } //end for loop
+    }) //end response
+} //end getTaskData function
+
+function postTaskData(){
+    console.log('button clicked');
+    let toDoList = {
+        task: $('#createTaskIn').val()
+    }; //end toDoList object
+    $ajax({
+        type: 'POST',
+        url: '/todo',
+        data: toDoList
+    }).then(function(response){
+        $('#createTaskIn').val('')
+        getTaskData();
+    });
 }
+
